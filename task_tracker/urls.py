3a5a5bf39a48a1dec89a57ce8954dba4a430ecc6
weiwensg <leung085@umn.edu>
2019-12-11 11:14:20 +0800

    Add hard-coded templates and Bootstrap

--- task_tracker/urls.py
+++ task_tracker/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='task_tracker-home')
+    path('', views.home, name='task_tracker-home'),
+    path('about', views.about, name='task_tracker-about'),
 ]
--- task_tracker/views.py
+++ task_tracker/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
+todos = [
+    {'name': 'Task 1 Name', 'description': 'Task 1 Description', 'status': 'TODO'},
+    {'name': 'Task 2 Name', 'description': 'Task 2 Description', 'status': 'TODO'},
+]
+ip = [
+    {'name': 'Task 3 Name', 'description': 'Task 3 Description', 'status': 'IP'},
+]
+done = [
+    {'name': 'Task 4 Name', 'description': 'Task 4 Description', 'status': 'DONE'},
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'task_tracker/home.html')
+    context = {
+        'todos': todos,
+        'ip': ip,
+        'done': done,
+    }
+    return render(request, 'task_tracker/home.html', context)
+
+def about(request):
+    return render(request, 'task_tracker/about.html')
